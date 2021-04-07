0a268e403e8fd105b69a49d0a4e27cd77f7bdd6b
unknown <rudrapatel26122004@gmail.com>
2021-04-07 15:04:57 -0400

    made a proper sleep timer

--- src/index.js
+++ src/index.js
@@ -13,13 +13,16 @@ client.on('message', async (msg) => {
   if (msg.channel.guild.id !== process.env.GUILDID) return
   if (!msg.content.startsWith(process.env.PREFIX)) return
   msg.channel.startTyping()
-  sleeptime = true
+  sleep()
   const translated = await translate(msg.content.slice(3), 'auto', 'en')
   msg.channel.stopTyping()
 
   msg.channel.send(translated)
 })
-
+function sleep (time) {
+  sleeptime = true
+  setTimeout(() => { sleeptime = false }, time)
+}
 let sleeptime
 
 client.login(process.env.TOKEN)
