c4a4d8512a68237fed5cccd40dec9daa8a4e679b
unknown <rudrapatel26122004@gmail.com>
2021-04-07 14:07:49 -0400

    added translate api

---
+++ src/translate-api/translate.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer')
+let browser; let page; const list = []; let ready = false
+
+// function pause(ms){return new Promise(r=>setTimeout(r,ms||100))}
+
+setInterval(async () => {
+  if (ready && list.length >= 1) {
+    ready = false
+    let [r, t, a, b] = list.shift()
+    if (`${t.length}` == '0') {
+      ready = true
+      return r('Empty message!')
+    }
+    const l = []
+    try {
+      t.match(/\|(.|\r|\n){1,}\|/g).forEach(w => {
+        const k = Math.random().toString(36).slice(2)
+        t = t.replace(w, k)
+        l.push([k, w])
+      })
+    } catch {}
+    await page.goto(`https://translate.google.com/?sl=${a}&tl=${b}&text=${t}&op=translate`)
+    // await pause()
+    t = await gettr()
+    l.forEach(a => t = t.replace(a[0], a[1]))
+    ready = true
+    r(t)
+  }
+}, 50)
+const gettr = () => {
+  return new Promise(async resolve => {
+    try {
+      const e = await page.$("[class='J0lOec']")
+      const t = await page.evaluate(e => e.innerText, e)
+      resolve(t.endsWith('verified_user') ? t.slice(0, -'verified_user'.length) : t)
+    } catch { gettr().then(resolve) }
+  })
+}
+;(async () => {
+  browser = await puppeteer.launch({ headless: false })
+  page = await browser.newPage()
+  ready = true
+})()
+module.exports = (a, b, c) => { return new Promise(resolve => list.push([resolve, a, b, c])) }
